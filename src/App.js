39b7dbef4683c8e662aea4aa4bc5574945212225
moitad12 <106792020+moitad12@users.noreply.github.com>
2023-01-12 21:32:45 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Counter from './Counter'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
+
